ac91b1f5170d8fb318e0eac20929043e01d47b1a
AayanAnwar <aayananwar26@gmail.com>
2024-11-22 16:01:21 +0500

    Added a sidebar, added multiple pages in one URL (no need to make multiple pages for everything).

---
+++ Code/pages/All_Chapters.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Page configuration
+st.set_page_config(
+    page_title="Chapter List",
+    page_icon=':scroll:',
+    layout='wide'
+)
+
+st.title("ALL CHAPTERS MASTER LIST")
+st.subheader("[Chapter 1](Chapter_1)")  # Link to doas1 (hidden page)
+st.write("NEXT CHAPTER WILL BE RELEASED ON 30/11/2024")
---
+++ chapterlist.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="All Chapters",
+    page_icon=':black_nib:',
+    layout='wide'
+)
+
+st.title("ALL CHAPTERS MASTER LIST")
+st.subheader("[Chapter 1](https://deathofashadow1.streamlit.app)")
